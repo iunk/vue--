19afec2b91ad57ff9183eebb168f973ea97c523a
fangwenfw <919130740@qq.com>
2019-10-21 21:27:14 +0800

    fw

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,9 @@ import ajax from "./ajax";
 
 //const BASE_URL='http://localhost:4000';
 const BASE_URL='/api';
+
 // 1、根据经纬度获取位置详情
-export const reqAddress = (geohash) => ajax(BASE_URL + `/position/$(geohash)`)
+export const reqAddress = (geohash) => ajax(`$(BASE_URL)/position/$(geohash)`)
 
 // 2、获取食品分类列表
 export const reqFoodTypes = () => ajax(BASE_URL + '/index_category')
