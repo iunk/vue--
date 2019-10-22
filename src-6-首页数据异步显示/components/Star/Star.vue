f11cf1ef81189ebb14b1019b3614ec7599ce90e8
fangwenfw <919130740@qq.com>
2019-10-22 14:03:42 +0800

    fw

---
+++ src-6-首页数据异步显示/components/Star/Star.vue
@@ -0,0 +1,37 @@
+<template>
+   <div class="star star-24">
+        <span class="star-item" v-for="(starClass, index) in starClasses" :key="index" :class="starClass"></span>
+   </div>
+</template>
+
+<script>
+  export default {
+    props:{
+        score: Number
+    },
+    computed: {
+        starClasses(){
+            const {score} = this
+            const spanArr=[]
+            const onNum = Math.floor(score)
+            for(let i = 0; i<onNum; i++){
+                spanArr.push('on')
+            }
+            if((score*10) % (onNum*10) >= 5){
+                spanArr.push('on')
+            }else{
+                spanArr.push('half')
+            }
+            while(spanArr.length < 5){
+                spanArr.push('off')
+            }
+            console.log(spanArr)
+            return spanArr
+        }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src-6-首页数据异步显示/store/actions.js
@@ -0,0 +1,37 @@
+import { 
+    RECEIVE_ADDRESS,
+    RECEIVE_CATEGORYS,
+    RECEIVE_SHOPS
+} from './mutations-types'
+import { 
+    reqAddress,
+    reqFoodCategorys,
+    reqShops
+} from '../api'
+
+export default {
+    async getAddress({commit, state}){
+        const goehash = state.latitude + ',' + state.longitude
+        const result = await reqAddress(goehash)
+        if(result.code === 0){
+            const address = result.data
+            commit(RECEIVE_ADDRESS, {address})
+        }
+    },
+    async getFoodCategorys({commit, state}){
+        const goehash = state.goehash
+        const result = await reqFoodCategorys(goehash)
+        if(result.code === 0){
+            const categorys = result.data
+            commit(RECEIVE_CATEGORYS, {categorys})
+        }
+    },
+    async getShops({commit,state}){
+        const {longitude, latitude} = state
+        const result = await reqShops({longitude, latitude})
+        if(result.code === 0){
+            const shops = result.data
+            commit(RECEIVE_SHOPS, {shops})
+        }
+    }
+}
